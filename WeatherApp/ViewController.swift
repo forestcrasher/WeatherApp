1962a1224ebda8d093197e1da80b9aa35b58307c
forestcrasher <forestcrasher@outlook.com>
2020-01-24 14:56:26 +0300

    Add updating Labels for info

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -12,32 +12,55 @@ let ACCESS_KEY = "db681f19d78408bc2a4236761f47920e"
 class ViewController: UIViewController {
 
     @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
     
-    override func viewDidLoad() {
+     override func viewDidLoad() {
         super.viewDidLoad()
-
         searchBar.delegate = self
     }
 }
 
 extension ViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(searchBar.text!)&appid=\(ACCESS_KEY)"
+        searchBar.resignFirstResponder()
         
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?q=\(searchBar.text!.replacingOccurrences(of: " ", with: "%20"))&appid=\(ACCESS_KEY)"
         let url = URL(string: urlString)
-        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+        
+        var city: String?
+        var temperature: String?
+        var errorHasOccured = false
+        
+        let task = URLSession.shared.dataTask(with: url!) { [weak self] (data, response, error) in
             do {
                 let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! [String: AnyObject]
                 
+                if let _ = json["cod"] {
+                    errorHasOccured = true
+                }
+                
                 if let name = json["name"] as? String {
-                    print(name);
+                    city = name
+                    errorHasOccured = false
                 }
                 
                 if let main = json["main"] {
                     let temp = main["temp"] as? Double
                     var tempC = temp! - 273.15
                     tempC.round()
-                    print(Int(tempC))
+                    temperature = "\(Int(tempC)) C"
+                }
+                
+                DispatchQueue.main.async {
+                    if errorHasOccured {
+                        self?.cityLabel.text = "Error has occured!"
+                        self?.temperatureLabel.isHidden = true
+                    } else {
+                        self?.cityLabel.text = city!
+                        self?.temperatureLabel.text = temperature!
+                        self?.temperatureLabel.isHidden = false
+                    }
                 }
             }
             catch let jsonError {
